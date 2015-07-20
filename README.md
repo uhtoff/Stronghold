cbef35b5bbee399094b76729579424c0279749dc
Russ Hewson <russ.hewson@gmail.com>
2015-07-20 01:06:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on July 17, 2015, 12:29 am.
---
+++ src/Meldon/StrongholdBundle/MeldonStrongholdBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Meldon\StrongholdBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MeldonStrongholdBundle extends Bundle
+{
+}
